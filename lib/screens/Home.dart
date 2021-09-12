05c5ef63dcfd1d94f7690d69023ba71bd07f708a
Sachin Singh <sachins@geekyants.com>
2021-09-12 16:36:41 +0530

    customize and build pizza buttons added in home scree

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -4,8 +4,10 @@ import 'package:coding_challenge_2021/services/size_config.dart';
 import 'package:coding_challenge_2021/utils/colors.dart';
 import 'package:coding_challenge_2021/utils/constants.dart';
 import 'package:coding_challenge_2021/utils/text_styles.dart';
+import 'package:coding_challenge_2021/view_models/pizza_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -20,132 +22,136 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-      floatingActionButton: TextButton(
-          onPressed: () {
-            SetupRoutes.push(context, Routes.DIYPIZZA);
-          },
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 40.toHeight),
-            alignment: Alignment.center,
-            width: SizeConfig().screenWidth - 50.toWidth,
-            height: 70.toHeight,
-            decoration: BoxDecoration(
-              color: ColorConstants.amber,
-              border: Border(),
-              borderRadius: BorderRadius.all(
-                Radius.circular(20.toWidth),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: ColorConstants.amber,
-                  blurRadius: 5,
-                  offset: Offset(-3.0, -0.2),
-                )
-              ],
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Customize your pizza',
-                  style: TextStyle(
-                    color: ColorConstants.white,
-                    fontSize: 20.toWidth,
-                  ),
-                ),
-              ],
-            ),
-          )),
+      floatingActionButton: Container(
+        child: Stack(children: [
+          Positioned(
+            child: Opacity(
+                key: UniqueKey(),
+                opacity: 0.5,
+                child: Container(
+                  height: 75,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                        ColorConstants.white,
+                        ColorConstants.black
+                      ])),
+                )),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              customButton('Customize pizza'),
+              customButton('Build pizza', isCustomBuild: true)
+            ],
+          ),
+        ]),
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: SafeArea(
         child: Container(
           padding: EdgeInsets.only(left: 10, right: 10, top: 40),
+          margin: EdgeInsets.only(bottom: 40),
           child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Center(
-                  child: Text(
-                    'DIY Pizza',
-                    style: CustomTextStyles.blackBold(size: 25),
-                  ),
-                ),
-                SizedBox(height: 20),
-                Container(
-                  width: 300.toWidth,
-                  color: Color(0xFFF7F7FF),
-                  child: Center(
-                    child: Row(
-                      children: <Widget>[
-                        Icon(Icons.search),
-                        Expanded(
-                          child: Container(
-                            width: 200.toWidth,
-                            padding: const EdgeInsets.only(left: 10),
-                            child: TextFormField(
-                              onChanged: (String str) {
-                                setState(() {
-                                  searchText = str;
-                                });
-                              },
-                              decoration: const InputDecoration(
-                                  hintText: 'Search your food',
-                                  hintStyle:
-                                      TextStyle(fontFamily: 'Montserrat'),
-                                  border: InputBorder.none),
-                            ),
-                          ),
-                        ),
-                        Icon(Icons.filter_alt_outlined)
-                      ],
+            child: Stack(children: [
+              Column(
+                children: <Widget>[
+                  Center(
+                    child: Text(
+                      'DIY Pizza',
+                      style: CustomTextStyles.blackBold(size: 25),
                     ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Color(0xFFFFC700),
-                      border: Border(),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20.toWidth),
-                      )),
-                  child: ClipRRect(
-                    clipBehavior: Clip.hardEdge,
-                    child: Transform.scale(
-                        scale: 0.8,
-                        child: Lottie.asset(Constants.HOME_BANNER_ANIMATION)),
+                  SizedBox(height: 20),
+                  Container(
+                    width: 300.toWidth,
+                    color: Color(0xFFF7F7FF),
+                    child: Center(
+                      child: Row(
+                        children: <Widget>[
+                          Icon(Icons.search),
+                          Expanded(
+                            child: Container(
+                              width: 200.toWidth,
+                              padding: const EdgeInsets.only(left: 10),
+                              child: TextFormField(
+                                onChanged: (String str) {
+                                  setState(() {
+                                    searchText = str;
+                                  });
+                                },
+                                decoration: const InputDecoration(
+                                    hintText: 'Search your food',
+                                    hintStyle:
+                                        TextStyle(fontFamily: 'Montserrat'),
+                                    border: InputBorder.none),
+                              ),
+                            ),
+                          ),
+                          Icon(Icons.filter_alt_outlined)
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Text('Hungry ? Quick order',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.left),
-                ),
-                SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 100.0),
-                  child: Wrap(
-                      alignment: WrapAlignment.start,
-                      runAlignment: WrapAlignment.start,
-                      runSpacing: 15.0,
-                      spacing: 20.0,
-                      children: List.generate(pizzaOptions.length, (index) {
-                        String pizzaName =
-                            pizzaOptions[index]['name'] as String;
-                        pizzaName = pizzaName.toLowerCase();
+                  SizedBox(height: 20),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xFFFFC700),
+                        border: Border(),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.toWidth),
+                        )),
+                    child: ClipRRect(
+                      clipBehavior: Clip.hardEdge,
+                      child: Transform.scale(
+                          scale: 0.8,
+                          child: Lottie.asset(Constants.HOME_BANNER_ANIMATION)),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text('Hungry ? Quick order',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left),
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 100.0),
+                    child: Wrap(
+                        alignment: WrapAlignment.start,
+                        runAlignment: WrapAlignment.start,
+                        runSpacing: 15.0,
+                        spacing: 20.0,
+                        children: List.generate(pizzaOptions.length, (index) {
+                          String pizzaName =
+                              pizzaOptions[index]['name'] as String;
+                          pizzaName = pizzaName.toLowerCase();
 
-                        if (pizzaName
-                            .contains(searchText.toLowerCase().trim())) {
-                          return pizzaCard(
-                              pizzaOptions[index]['path'] as String,
-                              pizzaOptions[index]['name'] as String);
-                        } else
-                          return SizedBox();
-                      })),
-                ),
-              ],
-            ),
+                          if (pizzaName
+                              .contains(searchText.toLowerCase().trim())) {
+                            return InkWell(
+                              onTap: () {
+                                final pizzaViewModel =
+                                    Provider.of<PizzaViewModel>(context,
+                                        listen: false);
+                                pizzaViewModel.selectedPizzaObj =
+                                    pizzaOptions[index];
+                                SetupRoutes.push(context, Routes.DIYPIZZA);
+                              },
+                              child: pizzaCard(
+                                  pizzaOptions[index]['path'] as String,
+                                  pizzaOptions[index]['name'] as String),
+                            );
+                          } else
+                            return SizedBox();
+                        })),
+                  ),
+                ],
+              ),
+            ]),
           ),
         ),
       ),
@@ -197,5 +203,44 @@ class _HomeState extends State<Home> {
       ),
     );
     return pizzaCard;
+  }
+
+  Widget customButton(String text, {bool isCustomBuild: false}) {
+    return InkWell(
+      onTap: () {
+        final pizzaViewModel =
+            Provider.of<PizzaViewModel>(context, listen: false);
+        pizzaViewModel.selectedPizzaObj = {};
+        SetupRoutes.push(context, Routes.DIYPIZZA);
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        alignment: Alignment.center,
+        width: 150,
+        height: 70.toHeight,
+        decoration: BoxDecoration(
+          color: isCustomBuild ? ColorConstants.white : ColorConstants.amber,
+          border: Border(),
+          borderRadius: BorderRadius.all(
+            Radius.circular(20.toWidth),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: ColorConstants.amber,
+              blurRadius: 2,
+              offset: Offset(-3.0, -0.2),
+            )
+          ],
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+              color:
+                  isCustomBuild ? ColorConstants.amber : ColorConstants.white,
+              fontSize: 15,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
